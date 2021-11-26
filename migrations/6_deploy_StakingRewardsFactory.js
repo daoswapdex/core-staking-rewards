8073f84f45ac6a25b349cba50a45275327957889
daoswap002 <daoswap@daoswap.cc>
2021-11-26 10:41:23 +0800

    deploy new period

--- migrations/6_deploy_StakingRewardsFactory.js
+++ migrations/6_deploy_StakingRewardsFactory.js
@@ -3,6 +3,6 @@ const StakingRewardsFactory = artifacts.require("StakingRewardsFactory");
 module.exports = function(deployer) {
     deployer.deploy(StakingRewardsFactory,
         '0xc096332CAacF00319703558988aD03eC6586e704', // 奖励代币地址
-        1636426800 // 激励奖励创世时间戳
+        1637895600 // 激励奖励创世时间戳
     );
 };
