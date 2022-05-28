d2439fca3433b6f6f3e140dc82fa3f9b6c78fd7b
daoswap002 <daoswap@daoswap.cc>
2022-05-28 21:01:07 +0800

    deploy period 7 on heco

--- migrations/51_deploy_StakingRewardsFactory_hecotestnet.js
+++ migrations/51_deploy_StakingRewardsFactory_hecotestnet.js
@@ -2,7 +2,7 @@ const StakingRewardsFactory = artifacts.require("StakingRewardsFactory");
 
 module.exports = function(deployer) {
     deployer.deploy(StakingRewardsFactory,
-        '0xc096332CAacF00319703558988aD03eC6586e704', // 奖励代币地址
-        1653793200 // 激励奖励创世时间戳
+        '0xd1c3d3cB6CE2D43aaBe8d1A78Dd5ADa60e5166c9', // 奖励代币地址
+        1653733200 // 激励奖励创世时间戳
     );
 };
